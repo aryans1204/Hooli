e7c757fe2eb249131855b2b26ab22da87dccf2b8
Aryan Sharma <arshar1204@gmail.com>
2023-01-26 01:40:25 +0800

    Added User endpoints: :

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -7,6 +7,29 @@ const auth = require('../authentication/auth')
 
 const router = new express.Router()
 
-//POST endpoint, body: {}
+//POST endpoint, create new user
+router.post('/api/users', async (req, res) => {
+    const user = new User(req.body)
+    
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//POST endpoint, sign in user
+router.post('/api/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await User.generateAuthToken()
+        res.send({user, token})
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
 
 
