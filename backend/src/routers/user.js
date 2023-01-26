bbce06e8ca8f4465baf6ce84d3e1737bcd30d9d1
Aryan Sharma <arshar1204@gmail.com>
2023-01-26 13:45:07 +0800

    Finished User endpoints

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -31,5 +31,67 @@ router.post('/api/users/login', async (req, res) => {
     }
 })
 
+//POST endpoint, log out user
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//POST endpoint to logout user from ALL devices
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//GET endpoint, get User profile
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+//PATCH endpoint, update my profile
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//DELETE endpoint, permanently remove a user
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
+
 
 
