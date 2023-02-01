934c59d2e25015e6750f063bd50b91964605ed76
jason-701 <jason.chang0701@gmail.com>
2023-02-01 15:31:30 +0800

    added logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@
 
 import { useState } from "react";
 import "./App.css";
-//import logo from "./assets/icons/hooli-logo.png"
+import logo from "./assets/icons/hooli-logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="screen">
+        <img class="logo" src={logo} alt="Hooli" />
         <div className="title">
           <h1>Welcome to Hooli</h1>
         </div>
