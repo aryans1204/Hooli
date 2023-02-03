cc95e4aac726275ff42be2d43d2515152678860b
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-04 00:24:01 +0800

    added usestate stuff

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
-import "./SignUp.css";
+import "./SignUp.module.css";
 import logo from "../assets/icons/hooli-logo.png";
 
-function signUp() {
+function SignUp() {
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
   });
 
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data.name + data.email + data.password);
+  };
+
   return (
-    <div className="App">
-      <div class="row">
-        <div class="column dark-purple">
-          <img class="logo" src={logo} alt="Hooli" />
+    <div className={"SignUp.app"}>
+      <div class={SignUp.row}>
+        <div class={SignUp.column + " " + SignUp.darkPurple}>
+          <img class={SignUp.logo} src={logo} alt="Hooli" />
         </div>
-        <div class="column">
-          <div class="login">
+        <div class={SignUp.column}>
+          <div class={SignUp.login}>
             <h2>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label>
                 <h3>Name</h3>
                 <input
@@ -34,19 +43,26 @@ function signUp() {
                 <h3>Email</h3>
                 <input
                   type="text"
-                  placeholder="name"
-                  name="name"
-                  value={data.name}
+                  placeholder="email"
+                  name="email"
+                  value={data.email}
                   required
                   onChange={handleChange}
                 />
               </label>
               <label>
                 <h3>Password</h3>
-                <input type="password" name="name" />
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  value={data.password}
+                  required
+                  onChange={handleChange}
+                />
               </label>
               <label>
-                <input type="submit" value="Log In" />
+                <button type="submit">Sign Up</button>
               </label>
             </form>
           </div>
@@ -56,4 +72,4 @@ function signUp() {
   );
 }
 
-export default signUp;
+export default SignUp;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./components/Login";
 import App from "./App";
+import SignUp from "./components/SignUp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App></App>
+    <SignUp />
   </React.StrictMode>
 );
