52b1a88daed12a51f8ad292575d6b0ae6da7fa70
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-08 01:06:08 +0800

    login.module.css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,35 +1,33 @@
 import { Component, useState } from "react";
-import "./Login.css";
+import classes from "./Login.module.css";
 import logo from "../assets/icons/hooli-logo.png";
 
 class Login extends Component {
   render() {
     return (
-      <div className="App">
-        <div class="row">
-          <div class="column dark-purple">
-            <img class="logo" src={logo} alt="Hooli" />
-          </div>
-          <div class="column">
-            <div class="login">
-              <h2>Sign In</h2>
-              <form>
-                <label>
-                  <h3>Email</h3>
-                  <input type="text" name="name" />
-                </label>
-                <label>
-                  <h3>Password</h3>
-                  <input type="password" name="name" />
-                </label>
-                <label>
-                  <input type="submit" value="Log In" />
-                </label>
-                <p>
-                  Don't have an account? <a href="#">Create An Account</a>
-                </p>
-              </form>
-            </div>
+      <div className={classes.row}>
+        <div className={classes.column + " " + classes["dark-purple"]}>
+          <img className={classes.logo} src={logo} alt="Hooli" />
+        </div>
+        <div className={classes.column}>
+          <div className={classes.login}>
+            <h2>Sign In</h2>
+            <form>
+              <label>
+                <h3>Email</h3>
+                <input type="text" name="name" />
+              </label>
+              <label>
+                <h3>Password</h3>
+                <input type="password" name="name" />
+              </label>
+              <label>
+                <input type="submit" value="Log In" />
+              </label>
+              <p>
+                Don't have an account? <a href="#">Create An Account</a>
+              </p>
+            </form>
           </div>
         </div>
       </div>
