9ab64c89583b307108de6566ab2397fdeca28ba7
Anika Tan <anika.tanyy@gmail.com>
2023-02-11 21:08:14 +0800

    Routing for pages in website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,13 @@ import { Routes, Route} from 'react-router-dom';
 import Welcome from "./components/Welcome";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
+import Expenditures from "./components/Expenditures";
+import Income from "./components/Income";
+import Investments from "./components/Investments";
+import Forex from "./components/Forex";
+// import PersonalProfile from "./components/PersonalProfile";
+
 
 function App() {
     return (
@@ -12,6 +19,13 @@ function App() {
         <Route exact path="/" element={<Welcome />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<SignUp />} />
+
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/expenditures" element={<Expenditures />} />
+        <Route exact path="/income" element={<Income />} />
+        <Route exact path="/investments" element={<Investments />} />
+        <Route exact path="/forex" element={<Forex />} />
+        {/* <Route exact path="/profile" element={<PersonalProfile />} /> */}
       </Routes>
     );
 
---
+++ frontend/src/components/Investments.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Investments extends Component {
+    render() {
+        return (
+            <h1>Investments Page</h1>
+        );
+    }
+}
+
+export default Investments;
