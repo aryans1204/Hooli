5401d1c4de1adfbd8c82cfc80804b9584f7455ee
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-11 13:28:52 +0800

    display profile data

--- frontend/src/components/PersonalProfile.jsx
+++ frontend/src/components/PersonalProfile.jsx
@@ -3,8 +3,38 @@ import classes from "./PersonalProfile.module.css";
 import logo from "../assets/icons/hooli-logo.png";
 
 class PersonalProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "" };
+  }
+
+  async componentDidMount() {
+    let token = sessionStorage.getItem("token");
+    let profileData = await fetch("/api/users/me", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let item = data;
+        return item;
+      });
+    this.setState({ name: profileData.name });
+    this.setState({ email: profileData.email });
+  }
   render() {
-    return <div className={classes.title}>Personal Profile</div>;
+    return (
+      <div>
+        <div className={classes.title}>Personal Profile</div>
+        <div>Name: {this.state.name}</div>
+        <div>Email: {this.state.email}</div>
+      </div>
+    );
   }
 }
 
