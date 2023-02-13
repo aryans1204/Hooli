abf12e076934ec388e5eddac6568daf3f0644d15
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-13 11:17:41 +0800

    import chakraui

--- frontend/src/components/Income.jsx
+++ frontend/src/components/Income.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import classes from "./Income.module.css";
+import { Box } from "@chakra-ui/react";
 
 class Income extends Component {
-    render() {
-        return (
-            <h1>Income Page</h1>
-        );
-    }
+  render() {
+    return (
+      <div className={classes.income}>
+        <div>
+          <NavBar />
+        </div>
+        <Box>this is a box</Box>
+        <div className={classes.contents}>
+          <div>Income page</div>
+          <div>test display</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Income;
