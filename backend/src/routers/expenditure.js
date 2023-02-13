e2f44a9964a7bd626e5e55e2e8de4d11c0acf153
Dayna Chia <day.chia@gmail.com>
2023-02-14 02:08:15 +0800

    added documentation for routers

--- backend/src/routers/expenditure.js
+++ backend/src/routers/expenditure.js
@@ -1,11 +1,43 @@
+/**
+ * express module
+ * @const
+ */
 const express = require('express')
+
+/**
+ * Expenditure module
+ * @const
+ */
 const Expenditure = require('../models/expenditure')
+
+/**
+ * Transaction module
+ * @const
+ */
 const Transaction = require('../models/transaction')
+
+/**
+ * auth module
+ * @const
+ */
 const auth = require('../authentication/auth')
 
+/**
+ * Express router to mount user related functions on.
+ * @type {object}
+ * @const
+ */
 const router = new express.Router()
 
-//GET endpoint to get all expenditures
+/**
+ * Route to get all expenditures.
+ * @name get/api/expenditure
+ * @async
+ * @param {String} path
+ * @param {Object} auth
+ * @param {callback} middleware
+ * @throws {InternalServerError}
+ */
 router.get('/api/expenditure', auth, async (req, res) => {
     try {
         await req.user.populate({
@@ -17,7 +49,15 @@ router.get('/api/expenditure', auth, async (req, res) => {
     } 
 })
 
-//GET endpoint for a particular expenditure by ID
+/**
+ * Route to get a particular expenditure by ID.
+ * @name get/api/expenditure/:id
+ * @async
+ * @param {String} path
+ * @param {Object} auth
+ * @param {callback} middleware
+ * @throws {InternalServerError}
+ */
 router.get('/api/expenditure/:id', auth, async (req, res) => {
     try {
         const expenditure = await Expenditure.findOne({ _id: req.params.id, expenditue_owner: req.user._id })
@@ -29,7 +69,15 @@ router.get('/api/expenditure/:id', auth, async (req, res) => {
     }
 })
 
-//POST endpoint for creating a new expenditure
+/**
+ * Route to create a new expenditure.
+ * @name post/api/expenditure
+ * @async
+ * @param {String} path
+ * @param {Object} auth
+ * @param {callback} middleware
+ * @throws {InternalServerError}
+ */
 router.post('/api/expenditure', auth, async (req, res) => {
     const expenditure = new Expenditure({
         expenditure_owner: req.user._id
