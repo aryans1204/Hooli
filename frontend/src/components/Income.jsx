3bb5c4cb16f6a3378e1340b230564a9bdf9c4741
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-14 13:45:14 +0800

    changed to class component

--- frontend/src/components/Income.jsx
+++ frontend/src/components/Income.jsx
@@ -13,85 +13,102 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-function Income() {
+export function AddOverlay() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className={classes.contents}>
-      <div>
-        <NavBar />
-      </div>
-      <div className={classes.title}>My Income</div>
-      <Box
-        bg="rgba(148, 114, 208, 1)"
-        w="769px"
-        h="400px"
-        color="white"
-        p={5}
-        mt={5}
-        ml="20"
+    <ButtonGroup spacing="40px" float="left" pl="180px">
+      <Button
+        onClick={onOpen}
+        w="175px"
+        h="71px"
         borderRadius="50"
+        color="white"
+        bg="#3f2371"
+        float="left"
       >
-        <div>test test test test</div>
-      </Box>
+        Add
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center" fontSize="30px">
+            Add new income
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            yes
+            <Box bg="gray">testing</Box>
+          </ModalBody>
 
-      <ButtonGroup spacing="40px" float="left" pl="180px">
-        <Button
-          onClick={onOpen}
-          w="175px"
-          h="71px"
-          borderRadius="50"
-          color="white"
-          bg="#3f2371"
-          float="left"
-        >
-          Add
-        </Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>yes</ModalBody>
-
-            <ModalFooter>
-              <Button
-                colorScheme="purple"
-                onClick={onClose}
-                h="50px"
-                w="80px"
-                d="flex"
-              >
-                Save
-              </Button>
-              <Button colorScheme="yellow" pl="20px">
-                Cancel
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-        <Button
-          w="175px"
-          h="71px"
-          borderRadius="50"
-          color="white"
-          bg="#3f2371"
-          float="left"
-        >
-          Remove
-        </Button>
-        <Button
-          w="175px"
-          h="71px"
-          borderRadius="50"
-          color="white"
-          bg="#3f2371"
-          float="left"
-        >
-          Edit
-        </Button>
-      </ButtonGroup>
-    </div>
+          <ModalFooter>
+            <Button
+              colorScheme="purple"
+              onClick={onClose}
+              h="50px"
+              w="80px"
+              d="flex"
+            >
+              Save
+            </Button>
+            <Button colorScheme="yellow" pl="20px">
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Button
+        w="175px"
+        h="71px"
+        borderRadius="50"
+        color="white"
+        bg="#3f2371"
+        float="left"
+      >
+        Remove
+      </Button>
+      <Button
+        w="175px"
+        h="71px"
+        borderRadius="50"
+        color="white"
+        bg="#3f2371"
+        float="left"
+      >
+        Edit
+      </Button>
+    </ButtonGroup>
   );
+}
+
+const AddOverlayComponent = ({ render }) => {
+  return AddOverlay();
+};
+
+class Income extends Component {
+  //const { isOpen, onOpen, onClose } = useDisclosure();
+  render() {
+    return (
+      <div className={classes.contents}>
+        <div>
+          <NavBar />
+        </div>
+        <div className={classes.title}>My Income</div>
+        <Box
+          bg="rgba(148, 114, 208, 1)"
+          w="769px"
+          h="400px"
+          color="white"
+          p={5}
+          mt={5}
+          ml="20"
+          borderRadius="50"
+        >
+          <div>test test test test</div>
+        </Box>
+        <AddOverlayComponent />
+      </div>
+    );
+  }
 }
 
 export default Income;
