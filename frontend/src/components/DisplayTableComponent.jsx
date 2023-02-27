ff6ce7e23b7ee63c203c42eab854ea07504cc036
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-02-27 21:47:09 +0800

    showing income data as table

---
+++ frontend/src/components/DisplayTableComponent.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function DisplayTableComponent(props) {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  function handleItemClick(item) {
+    setSelectedItem(item);
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Industry</th>
+          <th>Monthly Income</th>
+          <th>Start Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.items.map((item) => (
+          <tr key={item._id}>
+            <td>
+              <input
+                type="checkbox"
+                checked={selectedItem === item}
+                onChange={() => handleItemClick(item)}
+              />
+            </td>
+            <td>{item.industry}</td>
+            <td>{item.monthly_income}</td>
+            <td>{item.start_date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default DisplayTableComponent;
--- frontend/src/components/RemoveOverlayComponent.jsx
+++ frontend/src/components/RemoveOverlayComponent.jsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { Navigate } from "react-router-dom";
 import classes from "./RemoveOverlayComponent.module.css";
+import DisplayTableComponent from "./DisplayTableComponent";
 
 export function RemoveOverlay() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -61,7 +62,9 @@ export function RemoveOverlay() {
             Remove income data
           </ModalHeader>
           <ModalCloseButton />
-
+          <ModalBody>
+            <DisplayTableComponent items={result}></DisplayTableComponent>
+          </ModalBody>
           <ModalFooter>
             <Button
               colorScheme="purple"
