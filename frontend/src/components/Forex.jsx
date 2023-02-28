8a491a022dddeed1a3f961268adf50c214666126
Anika Tan <anika.tanyy@gmail.com>
2023-03-01 00:16:02 +0800

    getting api response and top 5 recent db

--- frontend/src/components/Forex.jsx
+++ frontend/src/components/Forex.jsx
@@ -2,7 +2,8 @@ import React, { Component, useState} from 'react';
 import { Navigate } from "react-router-dom";
 import NavBar from './NavBar';
 import classes from './Forex.module.css';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement, Table, Thead, Tbody, Tfoot, Tr, 
+    Th, Td, TableContainer,} from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons'
 
 class Forex extends Component {
@@ -13,6 +14,7 @@ class Forex extends Component {
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.postData = this.postData.bind(this);
         this.getAllData = this.getAllData.bind(this);
+        this.getPair = this.getPair.bind(this);
       }
 
     async componentDidMount() {
@@ -43,16 +45,35 @@ class Forex extends Component {
                 return element.trim();
               });
             console.log(arr);
-            let var1 = arr[0];
-            let var2 = arr[1];
-            this.setState({from: var1});
-            this.setState({to: var2}, ()=>{this.postData();});
+            let fromVar = arr[0];
+            let toVar = arr[1];
+            this.setState({from: fromVar});
+            this.setState({to: toVar}, ()=>{this.getPair(fromVar, toVar); this.postData();});
             //TODO: clear text in input
         }
     }
 
+    getPair(fromVar, toVar) {
+        var myHeaders = new Headers();
+        myHeaders.append("apikey", import.meta.env.VITE_FIXER_API_KEY);
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: myHeaders
+        };
+
+        var url = "https://api.apilayer.com/fixer/latest?base=" + fromVar + "&symbols=" + toVar;
+        //console.log(url);
+
+        fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+
+    // Post search to database
     postData() {
-        //const url = 'https://data.fixer.io/api/' + 'latest' + '?access_key=' + process.env.REACT_APP_FIXER_API_SECRET;
         fetch('/api/currencies', {
             method: 'POST',
             body: JSON.stringify({
@@ -71,8 +92,8 @@ class Forex extends Component {
              });
     }
 
+    // Get recent 5 searches
     getAllData () {
-        console.log("YOURE HERE");
         fetch('/api/currencies', {
             method: 'GET',
             headers: {
@@ -80,15 +101,25 @@ class Forex extends Component {
                 Authorization: `Bearer ${sessionStorage.getItem("token")}`
             }
         })
-        .then((response) => {
-            if (response.status == 500) console.log("Error");
-            return response.json;
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            var count = 0;
+            let fromArr = [];
+            let toArr = [];
+            data.forEach(element => {
+                if (count != 5) {
+                    count += 1;
+                    fromArr.push(element["currency_from"]);
+                    toArr.push(element["currency_to"]);
+                }
+            });
+            console.log(fromArr);
+            console.log(toArr);
         })
-        .then ((data) => {
-            console.log("YOURE THERE");
-            this.setState({items: data});
-            console.log(this.state);
-        })
+        .catch((err) => {
+            console.log(err.message);
+        });
     }
 
     render() {
@@ -105,8 +136,37 @@ class Forex extends Component {
                         children={<SearchIcon color='gray.600' />}
                     />
                     <Input placeholder='Enter Currency Pair' htmlSize={50} width='auto' variant='filled' onKeyDown={this.handleKeyDown}/>
-                    {this.state.items}
                     </InputGroup>
+                </div>
+                <div className={classes.currencyDiv}>
+                <TableContainer>
+                    <Table variant='simple'>
+                        <Thead>
+                        <Tr>
+                            <Th>Currency Pairs</Th>
+                            <Th>Rate</Th>
+                            <Th isNumeric>Fluctuation</Th>
+                        </Tr>
+                        </Thead>
+                        <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td isNumeric>25.4</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>feet</Td>
+                            <Td>centimetres (cm)</Td>
+                            <Td isNumeric>30.48</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>yards</Td>
+                            <Td>metres (m)</Td>
+                            <Td isNumeric>0.91444</Td>
+                        </Tr>
+                        </Tbody>
+                    </Table>
+                    </TableContainer>
                 </div>
             </div>
             </>
