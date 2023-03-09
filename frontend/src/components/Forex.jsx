dadd4ff2a6f47db878b5744ebd99ce3a64a70de1
Anika Tan <anika.tanyy@gmail.com>
2023-03-09 12:22:51 +0800

    added button, get 5 data from db

--- frontend/src/components/Forex.jsx
+++ frontend/src/components/Forex.jsx
@@ -2,17 +2,16 @@ import React, { Component, useState, useEffect} from 'react';
 import { Navigate } from "react-router-dom";
 import NavBar from './NavBar';
 import classes from './Forex.module.css';
-import { Input, InputGroup, InputLeftElement, Table, Thead, Tbody, Tfoot, Tr, 
-    Th, Td, TableContainer,} from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement, Button, ButtonGroup} from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import ForexTable from './ForexTable';
 
 class Forex extends Component {
     constructor(props) {
         super(props);
-        this.state = { from: "", to: "", authenticated: null, items: "", hasData: false};
+        this.state = { from: "", to: "", authenticated: null, hasData: false};
 
-        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleButton = this.handleButton.bind(this);
         this.postData = this.postData.bind(this);
         this.getAllData = this.getAllData.bind(this);
         this.getPair = this.getPair.bind(this);
@@ -37,14 +36,18 @@ class Forex extends Component {
           this.getAllData();
     }
 
-    handleKeyDown(event) {
-        if (event.key === 'Enter') {
-            let value = event.target.value;
+    handleButton(event) {
+        event.preventDefault();
+        let value = this.inputNode.value;
+        if (value.length == 0) {
+            alert("Empty input");
+        }
+        else {
             value = value.trim();
             let arr = value.split("/");
             arr = arr.map(element => {
                 return element.trim();
-              });
+            });
             console.log(arr);
             let fromVar = arr[0];
             let toVar = arr[1];
@@ -86,13 +89,11 @@ class Forex extends Component {
         };
 
         var url = "https://api.apilayer.com/fixer/latest?base=" + fromVar + "&symbols=" + toVar;
-        //console.log(url);
 
         fetch(url, requestOptions)
         .then(response => response.text())
         .then(result => {console.log("PAIR OK"); result = JSON.parse(result); arr.push(result);})
         .catch(error => console.log('error', error));
-
     }
 
     // Get recent 5 searches
@@ -110,25 +111,35 @@ class Forex extends Component {
             if (data.length != 0) {
                 this.setState({hasData: true});
                 // Gets recent 5 pairs and puts into conversions array
-                let count = data.count;
-                console.log(count);
-                // let count = 0;
-                // var conversions = [];
-                // data.forEach(element => {
-                //     if (count != 5) {
-                //         count += 1;
-                //         let pair = {from: element["currency_from"], to: element["currency_to"]}
-                //         conversions.push(pair);
-                //     }
-                // });
-                // // for each conversion, getPair and put into responses array
-                // var responses = [];
-                // conversions.forEach((item) => {
-                //     const from = item.from;
-                //     const to = item.to;
-                //     this.getPair(from, to, responses);
-                // });
-                // console.log(responses);
+                var conversions = []
+                // DB has <= 5 
+                if (data.length <= 5) {
+                    data.forEach(element => {
+                        let pair ={from: element["currency_from"], to: element["currency_to"]};
+                        conversions.push(pair);
+                    })
+                } else {
+                    // DB has > 5
+                    let count = 0;
+                    data.forEach(element => {
+                            if (count != 5) {
+                                count += 1;
+                                let pair = {from: element["currency_from"], to: element["currency_to"]}
+                                conversions.push(pair);
+                            }}
+                    )
+                }
+                console.log(conversions);
+                console.log("WE ARE HERE");
+
+                // for each conversion, getPair and put into responses array
+                var responses = [];
+                conversions.forEach((item) => {
+                    const from = item.from;
+                    const to = item.to;
+                    this.getPair(from, to, responses);
+                });
+                console.log(responses);
                     
                 // return (<p>sdgffgdgdfg</p>)
             }
@@ -157,30 +168,6 @@ class Forex extends Component {
         });
     }
 
-    //getTable () {
-    //     const responses = [];
-    //     allData = JSON.parse(allData);
-    //     allData.forEach((item) => {
-    //         const from = item.from;
-    //         const to = item.to;
-    //         responses.push(this.getPair(from, to));
-    //       });
-
-    //     return (
-    //         <Tr>
-    //             {responses.map(resp => {
-    //                 let pair = resp.base + '/' + Object.keys(resp.rates);
-    //                 return ( <>
-    //                     <Td>{pair}</Td>
-    //                     <Td>{resp.rates[0]}</Td>
-    //                     <Td>Something</Td>
-    //                     </>
-    //                 )
-    //             })}
-    //         </Tr>
-    //     )
-    //}
-
     render() {
         return (
             <>
@@ -194,7 +181,8 @@ class Forex extends Component {
                         pointerEvents='none'
                         children={<SearchIcon color='gray.600' />}
                     />
-                    <Input placeholder='Enter Currency Pair' htmlSize={50} width='auto' variant='filled' onKeyDown={this.handleKeyDown}/>
+                    <Input placeholder='Enter Currency Pair' htmlSize={50} width='auto' variant='filled' ref={node => (this.inputNode = node)}/>
+                    <Button colorScheme='purple' onClick={this.handleButton}>Search</Button>
                     </InputGroup>
                 </div>
                 {(this.state.hasData) ? (<ForexTable/>) : (<p>No entries yet!</p>)}
