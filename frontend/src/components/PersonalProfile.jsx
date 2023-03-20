b48ded702a76d2b18299ee4b520223a783c58c8f
Anika Tan <anika.tanyy@gmail.com>
2023-03-20 23:22:47 +0800

    changing name and email

--- frontend/src/components/PersonalProfile.jsx
+++ frontend/src/components/PersonalProfile.jsx
@@ -1,6 +1,9 @@
 import { Component, useState } from "react";
 import classes from "./PersonalProfile.module.css";
-import { Input, InputGroup, InputLeftElement, Button} from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement, Button, FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,} from '@chakra-ui/react';
 import { Navigate } from "react-router-dom";
 import  {DeleteAcc}  from "./ProfileComponents/DeleteAcc";
 import NavBar from "./NavBar";
@@ -11,10 +14,13 @@ class PersonalProfile extends Component {
     this.state = {
       name: "",
       email: "",
+      newName: "",
+      newEmail: "",
+      newPassword: "",
       authenticated: null,
-      inputName: '',
     };
-    // this.handleDeleteUser = this.handleDeleteUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount() {
@@ -37,18 +43,86 @@ class PersonalProfile extends Component {
       });
   }
 
+  handleChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    this.setState({[target.name]: target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    // Checks for name change
+    if ((this.state.name != this.state.newName) && (this.state.newName != "")) {
+      fetch("/api/users/me", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`
+        },
+        body: JSON.stringify({
+          name: this.state.newName,
+        })
+      })
+      .then ((response) => {
+        if (response.status == 400) console.log("Error updating profile name!");
+        else console.log("Profile name update successful");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }
+
+    // Checks for email change
+    if ((this.state.email != this.state.newEmail) && (this.state.newEmail != "")) {
+      fetch("/api/users/me", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`
+        },
+        body: JSON.stringify({
+          email: this.state.newEmail,
+        })
+      })
+      .then ((response) => {
+        if (response.status == 400) console.log("Error updating profile email!");
+        else console.log("Profile email update successful");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }
+    location.reload();
+  }
+
   render() {
     return (
       <>
         <NavBar />
         <div className={classes.contents}>
           <h1 className={classes.title}>Personal Profile</h1>
-          <div>Name: {this.state.name}</div>
-          <div>Email: {this.state.email}</div>
-          <Input placeholder='Enter Currency Pair' htmlSize={50} width='auto' variant='filled' id="myInput"/>
-          <br></br>
+
+          <form className={classes.details}>
+            <label htmlFor="fname">Name</label>
+            <input type="text" placeholder="Name" name="newName" defaultValue={this.state.name} onChange={this.handleChange} required></input>
+            <br/>
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Email" name="newEmail" defaultValue={this.state.email} required></input>
+            {/* <input type="submit" value="Change Details" onSubmit={this.handleSubmit}></input> */}
+            <Button colorScheme='purple' onClick={this.handleSubmit}>Change Details</Button>
+          </form>
+
+          <form className={classes.pwd}>
+            <label htmlFor="pwd">Enter New Password</label>
+            <input type="password" placeholder="New Password" onChange={this.handleChange} required></input>
+            <br/>
+            <label htmlFor="pwd">Confirm New Password</label>
+            <input type="password" placeholder="Confirm New Password" onChange={this.handleChange} required></input>
+            <input type="submit" value="Change Password"></input>
+          </form>
+
           <DeleteAcc />
-          {/* <Button colorScheme='purple' onClick={this.handleDeleteUser}>Delete Account</Button> */}
 
           <div>
             {this.state.authenticated == false && (
