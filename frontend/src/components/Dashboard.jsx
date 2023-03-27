7e7f6b5a998ea3d67a9d32c91ea7ec044dee552d
Anika Tan <anika.tanyy@gmail.com>
2023-03-27 11:38:45 +0800

    dividers

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -3,11 +3,52 @@ import classes from './Dashboard.module.css';
 import NavBar from './NavBar';
 
 class Dashboard extends Component {
+
+    /**
+     * Retrieves user profile and checks for authentiation when component is mounted
+     * @async
+     * @returns {*}
+     */
+    async componentDidMount() {
+        await fetch("/api/users/me", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        })
+          .then((response) => {
+            console.log(response.status);
+            if (response.status == 401)
+                this.setState({ authenticated: false });
+            else
+                this.setState({ authenticated: true });
+          });
+    }
+
     render() {
         return (
             <><NavBar />
-            <h1 className={classes.text}>Dashboard Page</h1>
-            
+            {this.state.authenticated == false && (<Navigate to="/" replace={true} />)}
+
+            <div className={classes.div}>
+                <h1 className={classes.text}>Dashboard Page</h1>
+                <div className={classes.container}>
+                    <div className={classes.weekly}>
+                        <p>sdfsdfs</p>
+                    </div>
+                    <div className={classes.breakdown}>
+                        <p>sdfsdfs</p>
+                    </div>
+                    <div className={classes.income}>
+                        <h2>Monthly Income Trend</h2>
+
+                    </div>
+                    <div className={classes.investments}>
+                        <p>sdfsdfs</p>
+                    </div>
+                </div>
+            </div>
             </>
         );
     }
