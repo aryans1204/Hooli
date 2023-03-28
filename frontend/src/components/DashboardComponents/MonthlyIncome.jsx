53f4bb3e61ed2a297d18db17a02e37c918c61ec7
Anika Tan <anika.tanyy@gmail.com>
2023-03-29 00:07:28 +0800

    Completed Monthly Income Graph

--- frontend/src/components/DashboardComponents/MonthlyIncome.jsx
+++ frontend/src/components/DashboardComponents/MonthlyIncome.jsx
@@ -19,38 +19,35 @@ function MonthlyIncome () {
 
     // Get income from the past 
     async function getYearIncome() {
-        try {
-          const response = await fetch('/api/income/', {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${sessionStorage.getItem("token")}`
-            }
-          });
-
-          const allData = await response.json();
-
-          if (allData.length != 0) {
-            setHasData(true);
-
-            // Get all income for the year
-            const year = new Date().getFullYear();
-
-            let yearData = allData.filter(data => ((data.start_date).includes(year)) == true)
-
-            var monthlyData = [{"Jan": 0}, {"Feb": 0}, {"Mar": 0}, {"Apr": 0}, {"May": 0}, {"Jun": 0}, {"Jul": 0}, {"Aug": 0}, {"Sep": 0}, {"Oct": 0}, {"Nov": 0}, {"Dec": 0}];
-
-            yearData.forEach(data => {
-              var monthIndex = Number(data.start_date.slice(5, 7)) - 1;
-              var month = monthlyData[monthIndex];
-              var monthTotal = month[Object.keys(month)];
-              month[Object.keys(month)] = monthTotal + data.monthly_income;
-            });
-            setIsLoading(false);
-            return monthlyData;
-          } else {
-            return -1;
+      try {
+        const response = await fetch('/api/income/', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${sessionStorage.getItem("token")}`
           }
+        });
+
+        const allData = await response.json();
+
+        if (allData.length != 0) {
+          setHasData(true);
+        }
+
+          // Get all income for the year
+          const year = new Date().getFullYear();
+
+          let yearData = allData.filter(data => ((data.start_date).includes(year)) == true)
+
+          var monthlyData = [{month: "Jan", income: 0}, {month: "Feb", income: 0}, {month: "Mar", income: 0}, {month: "Apr", income: 0}, {month: "May", income: 0}, {month: "Jun", income: 0}, {month: "Jul", income: 0}, {month: "Aug", income: 0}, {month: "Sep", income: 0}, {month: "Oct", income: 0}, {month: "Nov", income: 0}, {month: "Dec", income: 0}];
+
+          yearData.forEach(data => {
+            var monthIndex = Number(data.start_date.slice(5, 7)) - 1;
+            var month = monthlyData[monthIndex];
+            var monthTotal = month.income;
+            month.income = monthTotal + data.monthly_income;
+          });
+          return monthlyData;
         } catch (error) {
           console.error(error);
           throw new Error('Failed to fetch expenditures');
@@ -60,6 +57,7 @@ function MonthlyIncome () {
     async function getGraphData () {
         const yearIncome = await getYearIncome();
         setIncomeData(yearIncome);
+        setIsLoading(false);
     }
      
     if (isLoading) {
@@ -71,7 +69,7 @@ function MonthlyIncome () {
     let lowestValue = Number.MAX_SAFE_INTEGER;
     let highestValue = Number.MIN_SAFE_INTEGER;
     for (const data of incomeData) {
-      var value = data[Object.keys(data)];
+      var value = data.income;
       if (value < lowestValue) {
         lowestValue = value;
       }
@@ -79,8 +77,8 @@ function MonthlyIncome () {
         highestValue = value;
       }
     }
-    lowestValue = Math.floor(lowestValue * 1000) / 1000;
-    highestValue = Math.ceil(highestValue * 1000) / 1000
+    lowestValue = Math.floor(lowestValue * 100) / 100;
+    highestValue = Math.ceil(highestValue * 100) / 100 + 15;
 
     return (
       <div>
@@ -91,14 +89,14 @@ function MonthlyIncome () {
                   width={1000}
                   height={300}
                   data={incomeData}
-                  margin={{left: 70, right: 70}}
+                  margin={{left: 70, right: 70, bottom: 10}}
               >
                   <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="date" />
+                  <XAxis dataKey="month" />
                   <YAxis domain={[lowestValue, highestValue]}/>
                   <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="income" stroke="#8884d8" activeDot={{ r: 8 }} />
           </LineChart>
           </ResponsiveContainer>
           </>
