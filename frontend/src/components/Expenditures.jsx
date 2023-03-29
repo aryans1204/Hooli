77240d0e88645448b8df5444988c3e1db94ab967
jason-701 <jason.chang0701@gmail.com>
2023-03-29 13:17:51 +0800

    scrollbar to choose month

--- frontend/src/components/Expenditures.jsx
+++ frontend/src/components/Expenditures.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import NavBar from './NavBar';
-import classes from './Expenditures.module.css';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import classes from "./Expenditures.module.css";
 import { Navigate } from "react-router-dom";
 import { Box, Flex, Spacer, Text, Center } from "@chakra-ui/react";
-import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from "@chakra-ui/react";
 import { AddExpenditureComponent } from "./ExpendituresComponents/AddExpenditureComponent";
 import { EditExpenditureComponent } from "./ExpendituresComponents/EditExpenditureComponent";
 import { RemoveExpenditureComponent } from "./ExpendituresComponents/RemoveExpenditureComponent";
-import { ExpendituresPieChartComponent } from "./ExpendituresComponents/ExpendituresPieChartComponent";
+import {
+  ExpendituresPieChartComponent,
+  ScrollBar,
+} from "./ExpendituresComponents/ExpendituresPieChartComponent";
 import { DisplayExpendituresComponent } from "./ExpendituresComponents/DisplayExpendituresComponent";
-import DisplayAllExpendituresComponent from './ExpendituresComponents/DisplayAllExpendituresComponent';
+import DisplayAllExpendituresComponent from "./ExpendituresComponents/DisplayAllExpendituresComponent";
 
 /**
  * Expenditures class
@@ -18,7 +21,6 @@ import DisplayAllExpendituresComponent from './ExpendituresComponents/DisplayAll
  * @extends {Component}
  */
 class Expenditures extends Component {
-
   /**
    * Creates an instance of Expenditures.
    * @constructor
@@ -83,62 +85,86 @@ class Expenditures extends Component {
     return (
       <div className={classes.contents}>
         <div>
-          {this.state.authenticated == false && (<Navigate to="/" replace={true} />)}
+          {this.state.authenticated == false && (
+            <Navigate to="/" replace={true} />
+          )}
         </div>
         <div>
           <NavBar />
         </div>
         <div className={classes.title}>My Expenditures</div>
-        <Flex minWidth='max-content'>
-          <Center 
-            className={classes.chart} 
-            w='400px' 
-            border='2px' 
-            borderColor='#55185d' 
-            bg='white'
-            overflow="hidden">
+        <ScrollBar></ScrollBar>
+
+        <Flex minWidth="max-content">
+          <Center
+            className={classes.chart}
+            w="400px"
+            border="2px"
+            borderColor="#55185d"
+            bg="white"
+            overflow="hidden"
+          >
             <Box>
-              <Text className={classes.chartTitle}>Total Expenditures by Category </Text>
+              <Text className={classes.chartTitle}>
+                Total Expenditures by Category{" "}
+              </Text>
               {this.state.expendituresData !== null ? (
-                <ExpendituresPieChartComponent data={this.state.expendituresData} />
+                <ExpendituresPieChartComponent
+                  data={this.state.expendituresData}
+                />
               ) : null}
             </Box>
           </Center>
           <Spacer />
           <Center
-            w='250px' 
-            border='2px' 
-            borderColor='#55185d' 
-            overflow="hidden">
+            w="250px"
+            border="2px"
+            borderColor="#55185d"
+            overflow="hidden"
+          >
             <Box>
-              <Text className={classes.displayExpendituresTitle}>Recent Expenditures</Text>
+              <Text className={classes.displayExpendituresTitle}>
+                Recent Expenditures
+              </Text>
               {this.state.expendituresData !== null ? (
-                <DisplayExpendituresComponent data={this.state.expendituresData} />
+                <DisplayExpendituresComponent
+                  data={this.state.expendituresData}
+                />
               ) : null}
             </Box>
           </Center>
           <Spacer />
-          <VStack
-            spacing={4}
-            align='stretch'
-            width={450}
-            height={420}
-          >
-          <Box
-            border='2px' 
-            borderColor='#55185d' 
-            height={350}
-            overflowY="scroll">
-          {this.state.expendituresData !== null ? (
-              <DisplayAllExpendituresComponent items={this.state.expendituresData} />
-            ) : null}
-          </Box>
-          <Box>
-            <AddExpenditureComponent setState={() => {this.getData();}} />
-            <EditExpenditureComponent setState={() => {this.getData();}} />
-            <RemoveExpenditureComponent setState={() => {this.getData();}} />
-          </Box>
-        </VStack>
+          <VStack spacing={4} align="stretch" width={450} height={420}>
+            <Box
+              border="2px"
+              borderColor="#55185d"
+              height={350}
+              overflowY="scroll"
+            >
+              {this.state.expendituresData !== null ? (
+                <DisplayAllExpendituresComponent
+                  items={this.state.expendituresData}
+                />
+              ) : null}
+            </Box>
+            <Box>
+              <AddExpenditureComponent
+                setState={() => {
+                  this.getData();
+                }}
+              />
+              <EditExpenditureComponent
+                setState={() => {
+                  this.getData();
+                }}
+              />
+              <RemoveExpenditureComponent
+                setState={() => {
+                  this.getData();
+                }}
+              />
+            </Box>
+          </VStack>
         </Flex>
       </div>
     );
--- frontend/src/components/ExpendituresComponents/ExpendituresPieChartComponent.jsx
+++ frontend/src/components/ExpendituresComponents/ExpendituresPieChartComponent.jsx
@@ -1,7 +1,77 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { PieChart, Pie, Legend, Sector, Cell, ResponsiveContainer } from 'recharts';
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+} from "recharts";
+import classes from "./ExpendituresPieChartComponent.module.css";
 
+export const ScrollBar = () => {
+  const [startMonth, setStartMonth] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState(null);
+
+  const scrollLeft = () => {
+    if (startMonth > 0) {
+      setStartMonth(startMonth - 1);
+    }
+  };
+
+  const scrollRight = () => {
+    if (startMonth < 4) {
+      setStartMonth(startMonth + 1);
+    }
+  };
+
+  const handleMonthClick = (month) => {
+    setSelectedMonth(month);
+    console.log(month);
+  };
+
+  return (
+    <div className={classes.barContainer}>
+      <button
+        className={`${classes.left} ${classes.arrow}`}
+        onClick={scrollLeft}
+      ></button>
+      <div className={classes.selectBar}>
+        {Array.from({ length: 8 }, (_, i) => i + startMonth).map((i) => (
+          <div
+            key={i}
+            className={`${classes.option} ${
+              selectedMonth === i ? classes.selectedMonth : ""
+            }`}
+            onClick={() => handleMonthClick(i)}
+          >
+            {
+              [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+              ][i]
+            }
+          </div>
+        ))}
+      </div>
+      <button
+        className={`${classes.right} ${classes.arrow}`}
+        onClick={scrollRight}
+      ></button>
+    </div>
+  );
+};
 
 /**
  * Expenditures pie chart component
@@ -10,70 +80,98 @@ import { PieChart, Pie, Legend, Sector, Cell, ResponsiveContainer } from 'rechar
  * @returns {*}
  */
 export function ExpendituresPieChartComponent(props) {
-
   const [result, setResult] = useState(null);
-    useEffect(() => {
-      setResult(props.data);
-    }, [props.data]);
+  useEffect(() => {
+    setResult(props.data);
+  }, [props.data]);
 
   var pieData = [
-    {name: "Food", value: 0},
-    {name: "Housing", value: 0},
-    {name: "Utilities", value: 0},
-    {name: "Bills", value: 0},
-    {name: "Clothes", value: 0},
-    {name: "Lifestyle", value: 0},
-    {name: "Transport", value: 0},
-    {name: "Healthcare", value: 0},
-    {name: "Pets", value: 0},
-    {name: "Others", value: 0},
+    { name: "Food", value: 0 },
+    { name: "Housing", value: 0 },
+    { name: "Utilities", value: 0 },
+    { name: "Bills", value: 0 },
+    { name: "Clothes", value: 0 },
+    { name: "Lifestyle", value: 0 },
+    { name: "Transport", value: 0 },
+    { name: "Healthcare", value: 0 },
+    { name: "Pets", value: 0 },
+    { name: "Others", value: 0 },
   ];
 
   if (result !== null) {
-    result.forEach(({category, amount}) => {
-      var index = pieData.findIndex(x => x.name.toLowerCase() === category.toLowerCase());
-        pieData[index].value += amount;
+    result.forEach(({ category, amount }) => {
+      var index = pieData.findIndex(
+        (x) => x.name.toLowerCase() === category.toLowerCase()
+      );
+      pieData[index].value += amount;
     });
-  };
+  }
 
-  const COLORS = ['#37b067', '#6296bc', '#edb40d', '#7fd7c1', '#9f8cae', 
-                  '#eb6672', '#376c72', '#ee9dcc', '#e3791a', '#9f765e'];
- 
+  const COLORS = [
+    "#37b067",
+    "#6296bc",
+    "#edb40d",
+    "#7fd7c1",
+    "#9f8cae",
+    "#eb6672",
+    "#376c72",
+    "#ee9dcc",
+    "#e3791a",
+    "#9f765e",
+  ];
+
   const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
- 
+
     if (percent > 0) {
       return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
+        <text
+          x={x}
+          y={y}
+          fill="white"
+          textAnchor={x > cx ? "start" : "end"}
+          dominantBaseline="central"
+        >
+          {`${(percent * 100).toFixed(0)}%`}
         </text>
       );
-    };
+    }
   };
- 
+
   return (
     <div>
       <div>
         <div>
           <PieChart width={400} height={300}>
-          <Legend layout="vertical" verticalAlign="center" align="top" />
-          <Pie
-            data={pieData}
-            cx="60%"
-            cy="10%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={120}
-            fill="#8884d8"
-            dataKey="value"
-          >
-          {pieData.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-          </Pie>
+            <Legend layout="vertical" verticalAlign="center" align="top" />
+            <Pie
+              data={pieData}
+              cx="60%"
+              cy="10%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={120}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
           </PieChart>
         </div>
       </div>
